ee7f9bb9becf0d4f91d3004af00841a295ca5a09
Yamac Yurtsever <yamacyurtsever@hotmail.com>
2024-07-15 00:15:17 +1000

    finished room page layout

--- Frontend/src/pages/Room.tsx
+++ Frontend/src/pages/Room.tsx
@@ -1,9 +1,36 @@
 import './Room.css'
+import Chat from '../components/Chat'
 
 function Room() {
-    return (
-      <></>
-    )
+  return (
+    <div id='room-container'>
+      <WidgetButton/>
+      <ExitButton/>
+      <Chat/>
+    </div>
+  )
+}
+
+function WidgetButton() {
+  const openWidgetMenu = () => {
+    // TODO: Open the widget menu and make the button hide
+    console.log("Opened widget menu");
   }
+
+  return (
+    <div id="widget-button" onClick={openWidgetMenu}></div>
+  );
+}
+
+function ExitButton() {
+  const exitRoom = () => {
+    // TODO: Exit the room and go back to home page
+    console.log("Exited room");
+  }
+
+  return (
+    <div id="exit-button" onClick={exitRoom}>Exit Room</div>
+  );
+}
   
 export default Room
