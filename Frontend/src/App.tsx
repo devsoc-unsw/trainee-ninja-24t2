2ec221997f84089786c16947dfb624e62a4f14bf
Michael Tanto <michaeltanto0909@gmail.com>
2024-08-10 12:40:28 +0700

    Added ConnectForm component to Home page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,29 +1,28 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Room from './pages/Room'
-// import Home from './pages/Home'
 import { ConnectForm } from './components/ConnectForm';
-import AgoraRTC, {
-  AgoraRTCProvider,
-  useRTCClient,
-} from "agora-rtc-react";
 import { LiveChat } from './components/LiveChat';
+import Home from './pages/Home';
+import AgoraRTC, { AgoraRTCProvider, useRTCClient,
+} from "agora-rtc-react";
+
 function App() {
   const navigate = useNavigate();
 
   // Intialize Agora Client
   const agoraClient = useRTCClient( AgoraRTC.createClient({ codec: "vp8", mode: "rtc" })); // Initialize Agora Client
 
-  const handleConnect = (channelName: string) => {
-    // On form submit, navigate to new channel
-    navigate(`/via/${channelName}`) 
-  }
+  // const handleConnect = (channelName: string) => {
+  //   // On form submit, navigate to new channel
+  //   navigate(`/via/${channelName}`) 
+  // }
 
   return (
     // <Router>
     	<Routes>
-        {/* <Route path="/" element={<Home/>}/> */}
-        <Route path='/' element={ <ConnectForm connectToVideo={ handleConnect } /> } />
+        <Route path="/" element={<Home/>}/>
+        {/* <Route path='/' element={ <ConnectForm connectToVideo={ handleConnect } /> } /> */}
         <Route path="/room" element={<Room/>}/>
         <Route path='/via/:channelName' element={ 
           <AgoraRTCProvider client={agoraClient}>
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Router } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
