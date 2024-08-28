33ee2dd9d4ee0303118b8e2ad46c2461d9a57826
Langkee Hong <Langkee.Hong@gmail.com>
2024-08-29 00:49:57 +1200

    changed to TS and cleaned up code

---
+++ Backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import createNewRoom from "./newRoom.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGOURL: string = process.env.MONGO_URL || '';
+
+// start up server and connect to database
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log(`Connected to MongoDB: ${MONGOURL}`);
+    app.listen(PORT, () => {
+      console.log(`⚡️ Server started on port ${PORT} at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
+
+app.get('/', async (req, res) => {
+  res.send('hi');
+});
+
+app.get('/add/:username', (req, res) => {
+  createNewRoom(req, res);
+});
