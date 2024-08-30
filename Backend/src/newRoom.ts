44f955b08d38d451c419b7991cee539ae3bea4ce
Langkee Hong <z5492325@ad.unsw.edu.au>
2024-08-30 13:45:24 +1200

    changed room creation logic

--- Backend/src/newRoom.ts
+++ Backend/src/newRoom.ts
@@ -4,18 +4,19 @@ import generateRandomString from "./randomId.js";
 
 async function createNewRoom(req: Request, res: Response) {
   const username: string = req.params.username;
+  const roomId = generateRandomString();
   console.log(username);
 
   try {
     // Check if a room with the same roomId already exists
-    const existingRoom = await Room.findOne({ username });
+    const existingRoom = await Room.findOne({ roomId: roomId });
     if (existingRoom) {
       res.status(400).send({ message: 'Room with this ID already exists' });
     }
 
     // Create a new room since no duplicate was found
     const room = new Room({
-      roomId: generateRandomString(),
+      roomId: roomId,
       users: [username]
     });
 
