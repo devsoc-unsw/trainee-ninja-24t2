883f7dbdcac17add6a9d04f8165193201870ac36
Langkee Hong <Langkee.Hong@gmail.com>
2024-09-02 16:35:04 +1200

    michael pushed and i wasn't done yet

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -21,28 +21,28 @@ mongoose
   .connect(MONGOURL)
   .then(() => {
     console.log(`Connected to MongoDB: ${MONGOURL}`);
+    
     app.listen(PORT, () => {
       console.log(`⚡️ Server started on port ${PORT} at http://localhost:${PORT}`);
     });
 
-    io.on('connection', (socket) => {
-      console.log("user connected");
-      socket.on('message', (message) => {
-        console.log(message);
-        io.emit('message', `i said ${message}`)
-      });
+    http.listen(SOCKET, () => {
+      console.log(`listening on http://localhost:${SOCKET}`);
     });
   })
   .catch((error) => console.log(error));
 
-app.get('/', async (req, res) => {
-  res.send('hi');
-});
-
-app.get('/add/:username', (req, res) => {
+app.post('/create', (req, res) => {
   createNewRoom(req, res);
 });
 
-http.listen(SOCKET, () => {
-  console.log(`listening on http://localhost:${SOCKET}`);
+io.on('connection', (socket) => {
+  console.log("user connected");
+  socket.on('joinRoom', (roomId) => {
+    socket.join(roomId);
+    console.log(roomId);
+
+    const clients = io.sockets.adapter.rooms.get(roomId);
+    io.to(roomId).emit("userJoin", clients.size);
+  });
 });
